45dc840d422a82d074cee32f9416e4069d0a014d
Ahmad Saleem <ahmadsaleem93@gmail.com>
2015-08-17 16:51:18 +0500

    User: First Name and Last Name fields added in 'New' and 'Edit' actions

--- db/migrate/20150817104606_devise_create_users.rb
+++ db/migrate/20150817104606_devise_create_users.rb
@@ -30,8 +30,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :fname
-      t.string :lname
+      t.string :first_name, limit: 30
+      t.string :last_name, limit: 30
 
       t.timestamps null: false
     end
