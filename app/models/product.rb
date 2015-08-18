94758bfd2c9b39c199ee21d39ee74d06490ff398
Ahmad Saleem <ahmadsaleem93@gmail.com>
2015-08-18 19:34:36 +0500

    Product: Add photos functionality added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ActiveRecord::Base
-  attr_accessible :body, :price, :title
+
+  has_many :images, as: :imageable, dependent: :destroy
+
+  attr_accessible :body, :price, :title, :images_attributes
+
+  accepts_nested_attributes_for :images, allow_destroy: true
+
 end
