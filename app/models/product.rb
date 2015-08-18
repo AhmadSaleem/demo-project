e53573b00964543dd7777649b2b3b9e063756cc6
Ahmad Saleem <ahmadsaleem93@gmail.com>
2015-08-18 19:57:52 +0500

    Product: Pagination on index implemented

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,13 @@
 class Product < ActiveRecord::Base
 
+  PER_PAGE_SIZE = 5
+
   has_many :images, as: :imageable, dependent: :destroy
 
   attr_accessible :body, :price, :title, :images_attributes
 
   accepts_nested_attributes_for :images, allow_destroy: true
+
+  scope :ordered, -> { order("created_at desc") }
 
 end
