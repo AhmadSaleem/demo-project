0ed4f894a967c4961def53449a78fb905aae9f0d
Ahmad Saleem <ahmadsaleem93@gmail.com>
2015-08-19 16:27:16 +0500

    Product:Validations added.

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,6 +8,9 @@ class Product < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
 
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :price, presence: true, numericality: { greater_than: 0 }
+
   scope :ordered, -> { order("created_at desc") }
 
 end
