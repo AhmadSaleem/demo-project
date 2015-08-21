5e91bee9b020978a07d9d24ea58b7d1f4f4546c8
Ahmad Saleem <ahmadsaleem93@gmail.com>
2015-08-21 11:48:41 +0500

    ActiveAdmin:'id' and 'created_at' columns removed from product.

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -3,14 +3,11 @@ ActiveAdmin.register Product do
   actions :all, :except => [:edit]
 
   index do
-    column :id
     column :title
     column :body
     column :price, sortable: :price do |product|
     number_to_currency product.price
   end
-
-  column :created_at
 
   column :reviews do |review|
     link_to "reviews", admin_product_reviews_path(review)
