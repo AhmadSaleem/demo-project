c87aeee5a51785a6a58452dc21b4f48fd7a7daef
Ahmad Saleem <ahmadsaleem93@gmail.com>
2015-08-24 18:37:19 +0500

    Styling added to all pages through bootstrap.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def authorized_user?(user)
     user_signed_in? && current_user == user
   end
+
+  def render_name(user)
+    [user.first_name, user.last_name].join(" ")
+  end
+
+  def render_photo(image)
+    return if image.blank?
+    image.photo.url(:small)
+  end
+
 end
