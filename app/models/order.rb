d89b77ff95be3716ed21a4a9048201b924a60747
Ahmad Saleem <ahmadsaleem93@gmail.com>
2015-08-25 15:57:51 +0500

    Issue in association resolved

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,8 +3,8 @@ class Order < ActiveRecord::Base
   attr_accessible :discount, :total_price, :user_id
 
   belongs_to :user
+  has_many :order_products, dependent: :destroy
   has_many :products, through: :order_products
-  has_many :order_products, through: :order_products, dependent: :destroy
 
   validates :discount, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :total_price, presence: true, numericality: { greater_than_or_equal_to: 0 }
