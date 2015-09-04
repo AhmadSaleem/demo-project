dc43e88ed2b42aa109f1e36178112ea0eaed56fc
Ahmad Saleem <ahmadsaleem93@gmail.com>
2015-09-04 13:27:52 +0500

    Review: Truncation logic updated.

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,6 +1,7 @@
 module ReviewsHelper
+
   def render_review(review)
-    return review.body[0..499] + "..." if review.body.length > 500
-    return review.body
+    review.body.truncate(200)
   end
+
 end
