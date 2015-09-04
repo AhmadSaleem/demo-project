2ae41062da45e2d7d3564b0bf8843f4b2e7a11e3
Ahmad Saleem <ahmadsaleem93@gmail.com>
2015-09-04 12:48:59 +0500

    Product: Title truncated and styling improved on index page.

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,6 +1,12 @@
 module ProductsHelper
+
   def render_description(product)
     return "No description found." if product.body.blank?
     return product.body
   end
+
+  def render_title(product)
+    product.title.truncate(15)
+  end
+
 end
