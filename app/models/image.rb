7542078065d30228a087a0349a6c0786b7505cc4
Ahmad Saleem <ahmadsaleem93@gmail.com>
2015-09-04 13:28:48 +0500

    Image: Style for small image changed.

--- app/models/image.rb
+++ app/models/image.rb
@@ -7,7 +7,7 @@ class Image < ActiveRecord::Base
   belongs_to :imageable, polymorphic: true
   has_attached_file :photo,
     styles: {
-      small: "100x100>",
+      small: "100x100#",
       medium: "300x300>",
       full: "500x500>",
     },
